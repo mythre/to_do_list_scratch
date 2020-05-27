763c328e4d1f4b483d646be76baa4f9490082c54
Mythreyi.K <mythreyi1509@gmail.com>
2020-05-27 13:40:47 +0530

    Passing userid as parameter in jersey and using classes

--- src/main/java/com/todo/api/ToDoAPI.java
+++ src/main/java/com/todo/api/ToDoAPI.java
@@ -1,34 +1,38 @@
 package com.todo.api;
 
-import com.todo.impl.DatabaseConfig;
-import com.todo.impl.ListDAO;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.todo.model.Todo;
+import com.todo.impl.TodoDAO;
+import com.todo.impl.TodoDAOImpl;
 import com.google.gson.Gson;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.Produces;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import javax.ws.rs.core.MediaType;
 
 @Path("/todos")
 @Produces(MediaType.APPLICATION_JSON)
 public class ToDoAPI {
+    TodoDAO todoDAO;
+    public ToDoAPI()
+    {
+        todoDAO = new TodoDAOImpl();
+    }
 
     @GET
-    public Response getTodos() {
-        try {
-            ApplicationContext ctx = new AnnotationConfigApplicationContext(DatabaseConfig.class);
-            ListDAO list_obj = ctx.getBean(ListDAO.class);
-            ResultSet Result = list_obj.getTasksByUserId(1);
+    @Path("/{userId}")
+    public Response getTodos(@PathParam("userId") Integer userId) {
+
+            List<Todo> todos = todoDAO.listAllTodos(userId);
             Gson gson = new Gson();
-            String result_json = gson.toJson(Result.next());
+            String result_json = gson.toJson(todos);
             return Response.ok().entity(result_json).build();
-        } catch (SQLException sq) {
-            sq.printStackTrace();
-            return Response.ok().entity("error").build();
+
         }
     }
-}
+
